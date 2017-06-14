413b8eb377e14856a2b24eada55a913d27d07836
andres <anddres.arce@gmail.com>
2017-06-14 13:08:30 -0400

    [add] GameManager / BoardManager / Player Script / Wall Script
    Refactory to new version 5.6.1

--- RogueLike/Assets/Scripts/GameManager.cs
+++ RogueLike/Assets/Scripts/GameManager.cs
@@ -2,14 +2,39 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
+    public BoardManagerTutorial boardScript;
+    //Singleton
+    public static GameManager instance = null;
 
-	// Use this for initialization
-	void Start () {
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
+
+
+    private int level = 3;
+    //API - Métodos
+    void Awake()
+    {
+        //Chequea si ya existe una instancia de tipo GameManager porque debe 
+        //ser sólo 1 por escena.
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        //Hace que el objeto no se destruya al cargar una nueva escena.
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManagerTutorial>();
+        InitGame();
+    } 
+
+    public void GameOver()
+    {
+        enabled = false;
+    }
+        
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
---
+++ RogueLike/Assets/Scripts/Player.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MovingObject {
+
+
+    //Variables
+    public int wallDamage = 1;
+    public int pointsPerFood = 10;
+    public int pointsPerSoda = 20;
+    public float restartLevelDelay = 1f;
+
+    private Animator animator;
+    private int food;
+
+    //------------API - Métodos------------------------------
+
+    //Inicio, método sobreescrito de la herencia de MovingObject.
+	protected override void Start () {
+        animator = GetComponent<Animator>();
+        food = GameManager.instance.playerFoodPoints;
+        base.Start();
+	}
+
+    //se ejecuta 1 vez por frame
+    void Update()
+    {
+        if (!GameManager.instance.playersTurn)
+        {
+            return;
+        }
+        
+        int horizontal = 0;
+        int vertical = 0;
+
+        horizontal = (int)Input.GetAxisRaw("Horizontal");
+        vertical = (int)Input.GetAxisRaw("Vertical");
+
+        //Esto es para prevenir diagonales, o se mueve horizontalmente, o verticalmente.
+        if (horizontal != 0)
+        {
+            vertical = 0;
+        }
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            AttemptMove<Wall>(horizontal, vertical);
+        }
+    }
+    private void OnDisable()
+    {
+        GameManager.instance.playerFoodPoints = food;
+    }
+
+    //Colisión 2D que dependiendo del tag del objeto ejecutará distintivos métodos.
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Exit")
+        {
+            Invoke("Restart", restartLevelDelay);
+            enabled = false;
+        }
+
+        if (collision.tag == "Food")
+        {
+            food += pointsPerFood;
+            collision.gameObject.SetActive(false);
+        }
+        if (collision.tag == "Soda")
+        {
+            food += pointsPerSoda;
+            collision.gameObject.SetActive(false);
+        }
+    }
+
+    //------------------Métodos custom de esta clase------------------------------------
+
+    //Método sobeescrito de la herencia para intentar moverse.
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        food--;
+        base.AttemptMove<T>(xDir, yDir);
+        RaycastHit2D hit;
+
+        CheckIfGameOver();
+
+        GameManager.instance.playersTurn = false;
+    }
+
+    //chequea si no tienes comida y termina el juego de ser cierto.
+    private void CheckIfGameOver()
+    {
+        if (food<=0)
+        {
+            GameManager.instance.GameOver();
+        }
+    }
+
+    //Método sobreescrito de la herencia para cuando no puede moverse por encontrarse
+    //con una "pared" (asumiendo que el metodo abstracto se encontró con una pared).
+    protected override void OnCantMove<T>(T component)
+    {
+        Wall hitWall = component as Wall;
+        hitWall.DamageWall(wallDamage);
+        animator.SetTrigger("playerChop");
+    }
+
+    //reinicio del nivel
+    private void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    //pérdida de comida
+    public void LoseFood (int loss)
+    {
+        animator.SetTrigger("playerHit");
+        food -= loss;
+        CheckIfGameOver();
+    }
+
+}
